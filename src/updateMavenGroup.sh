3ccc482919e8c5e9d61bd9bb39feb33ea7b4b54a
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2016-04-20 19:00:52 +0200

    upgrade src folder

--- src/updateMavenGroup.sh
+++ src/updateMavenGroup.sh
@@ -4,15 +4,26 @@
     set -e
     set -o pipefail
 
+ORIGIN_FOLDER=$(pwd)
+
 echo rename source folder
-find ./ -type d -name 'dc' | grep .git -v | grep target -v
 
-echo rename packages
-sed -i 's/.dc./.openbase./g' *.java
+SOURCE_FOLDER=$(find ./ -type d -name 'dc' | grep .git -v | grep target -v)
 
-echo rename artifacts
-sed -i 's/dc/openbase/g' *.pom
+for DIR in ${SOURCE_FOLDER}
+do
+   echo [$DIR]
+   cd ${DIR}/..
+   git mv dc openbase
+   cd ${ORIGIN_FOLDER}
+done
 
-echo rename cooperation namw
-sed -i 's/DivineCooperation/openbase.org/g' *.pom
+#echo rename packages
+#sed -i 's/.dc./.openbase./g' *.java
+
+#echo rename artifacts
+#sed -i 's/dc/openbase/g' *.pom
+
+#echo rename cooperation namw
+#sed -i 's/DivineCooperation/openbase.org/g' *.pom
 
