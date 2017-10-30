80303f1b490bc900b07e46056e5ce2b672add6a9
Divine Threepwood <divine@openbase.org>
2017-10-30 17:30:47 +0100

    install script optimized.

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+WHITE='\033[0;37m'
+
 APP_NAME='developer-tools'
-clear &&
-echo "=== install ${APP_NAME} ===" &&
-install -t ${prefix}/bin src/*
-clear &&
-echo "=== ${APP_NAME} is successfully installed to ${prefix} ==="
+APP_NAME=${BLUE}${APP_NAME}${NC}
+echo -e "=== ${APP_NAME} project ${WHITE}installation${NC}" &&
+install -t ${prefix}/bin src/* &&
+echo -e "=== ${APP_NAME} was ${GREEN}successfully${NC} installed to ${WHITE}${prefix}${NC}"
