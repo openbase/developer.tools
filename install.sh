b845132381fcefa103937b37b845a0df54464c51
Divine Threepwood <divine@openbase.org>
2021-12-31 15:33:05 +0100

    update install script to target openbase dist instead of generic prefix.

--- install.sh
+++ install.sh
@@ -7,8 +7,18 @@ ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 WHITE='\033[0;37m'
 
+export BCO_DIST="${BCO_DIST:=$HOME/usr/}"
+export OPENBASE_DIST="${OPENBASE_DIST:=$BCO_DIST}"
+
+if [ ! -d ${OPENBASE_DIST} ]; then
+    echo "No openbase distribution found at: ${OPENBASE_DIST}"
+    echo "Please define the distribution installation target directory by setting the $OPENBASE_DIST environment variable."
+    exit 255
+fi
+
 APP_NAME='developer-tools'
 APP_NAME=${BLUE}${APP_NAME}${NC}
 echo -e "=== ${APP_NAME} project ${WHITE}installation${NC}" &&
-rsync -a src/* ${prefix}/bin/ &&
-echo -e "=== ${APP_NAME} was ${GREEN}successfully${NC} installed to ${WHITE}${prefix}${NC}"
+mkdir -p ${OPENBASE_DIST}/bin &&
+rsync -a src/* ${OPENBASE_DIST}/bin/ &&
+echo -e "=== ${APP_NAME} was ${GREEN}successfully${NC} installed to ${WHITE}${OPENBASE_DIST}${NC}"
